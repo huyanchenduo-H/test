394e73e757be1dbe59bf2768ff5ecd091b450acb
huyanchenduo-H <huyanchenduo@163.com>
2019-10-26 17:50:38 +0800

    新建了一个文件

---
+++ src/tset.java
@@ -0,0 +1,23 @@
+public class tset {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+
+
+        }
+
+    }
+}
+
+            
+
+       
+
+            
+
+     
+            
+            
+
+
+
+
